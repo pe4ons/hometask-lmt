cadc88b85c52011af4cfe5da7e85a4e5bcc4f3f4
Peteris <peteris.cibula@gmail.com>
2025-06-05 20:16:25 +0300

    add tests

--- tests/observationForm.spec.ts
+++ tests/observationForm.spec.ts
@@ -1,11 +1,29 @@
 import { test, expect } from '@playwright/test';
 import { ObservationPage } from '../page/observationPage';
+import { defaultInputData } from '../data/formData';
 
-test.describe('foo', () => {
-    test('bar', async ({ page }) => {
+test.describe('observation form', () => {
+    test.beforeEach(async ({ page }) => {
+        const observationPage = new ObservationPage(page);
+        await observationPage.openObservationPage();
+        await observationPage.newObservationToggle.click();
+    });
+
+    test('adding new observation', async ({ page }) => {
         const observationPage = new ObservationPage(page);
 
-        await page.goto('http://localhost:8000/');
-        await page.waitForTimeout(200);
-    })
+        await observationPage.fillForm();
+        await observationPage.addBtn.click();
+        const firstText = page.getByText(defaultInputData.location).first();
+        await expect(firstText).toBeVisible();
+    });
+
+    test('delete observation', async ({ page }) => {
+        const observationPage = new ObservationPage(page);
+
+        await observationPage.fillForm({ location: 'Delete me' });
+        await observationPage.addBtn.click();
+        await observationPage.deleteBtn.last().click();
+        await expect(page.getByText('Delete me')).not.toBeVisible();
+    });
 });
